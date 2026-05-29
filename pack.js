6f8839f78e241dd1567df867fe862d93490472cb
Nitai <nitai@helpmonks.com>
2026-05-28 22:19:43 -0400

    New build with email extensions

--- pack.js
+++ pack.js
@@ -1,32 +1,37 @@
 const fs = require('fs');
-const archiver = require('archiver');
 const path = require('path');
 
 const buildDir = 'build';
 const outputFileName = 'build.zip';
 
-const output = fs.createWriteStream(path.join(__dirname, outputFileName));
-const archive = archiver('zip', {
-  zlib: { level: 9 }
-});
+async function pack() {
+  const { ZipArchive } = await import('archiver');
 
-output.on('close', function() {
-  console.log(`${archive.pointer()} total bytes`);
-  console.log(`Chrome extension packaged as ${outputFileName}`);
-});
+  const output = fs.createWriteStream(path.join(__dirname, outputFileName));
+  const archive = new ZipArchive({
+    zlib: { level: 9 }
+  });
 
-archive.on('warning', function(err) {
-  if (err.code === 'ENOENT') {
-    console.warn(err);
-  } else {
+  output.on('close', function() {
+    console.log(`${archive.pointer()} total bytes`);
+    console.log(`Chrome extension packaged as ${outputFileName}`);
+  });
+
+  archive.on('warning', function(err) {
+    if (err.code === 'ENOENT') {
+      console.warn(err);
+    } else {
+      throw err;
+    }
+  });
+
+  archive.on('error', function(err) {
     throw err;
-  }
-});
+  });
 
-archive.on('error', function(err) {
-  throw err;
-});
+  archive.pipe(output);
+  archive.directory(buildDir, false);
+  archive.finalize();
+}
 
-archive.pipe(output);
-archive.directory(buildDir, false);
-archive.finalize();
+pack();
